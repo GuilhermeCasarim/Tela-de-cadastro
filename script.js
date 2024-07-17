cd270f6d7a9e5bdb914625f581929fce413650e6
GuilhermeCasarim <140670537+GuilhermeCasarim@users.noreply.github.com>
2024-07-17 15:27:24 -0300

    primeiro commit postando projeto vanilla

---
+++ script.js
@@ -0,0 +1,19 @@
+//identificacao dom
+// const btn = document.getElementById('btn')
+
+
+//funcao
+
+// //addeventlistener
+// btn.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     const inputs = document.getElementsByTagName('input')
+//     inputs.forEach((input) => {
+//         let input = input.value
+//         if(input){
+//             alert('input ruim')
+//         } else{
+//             alert('input bom')
+//         }
+//     })
+// })
